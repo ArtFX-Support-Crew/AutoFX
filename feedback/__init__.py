57d071fe260bc1fb65e795e692a1a76970e57a7f
ropeadope62 <davechadwick@gmail.com>
2023-04-04 00:07:47 -0400

    Initial commit, basic functionality
    The code lays the foundations for feedback post enforcement, enforcing channel, character minimum and keywords.
    
    Karma system - When a user posts a message that is a reply to a feedback post and the reply content qualifies as feedback. The user is awarded 1 karma which is appended to karma.json, a dictionary mapping user ids to thread ids.
    The user can only receive 1 karma per thread.
    
    Keyword system - keywords are enforced, mix feedback should contain key words which are common in music production and sound design.
    
    Some basic logging has been added in the form of print statements, this will be improved later.,
    
    - Included code to prevent feedback poster receiving karma from replies to their own thread

--- feedback/__init__.py
+++
@@ -1,4 +0,0 @@
-from .feedback import Feedback
-
-async def setup(bot):
-    bot.add_cog(Feedback(bot))
--- feedback/feedback.py
+++ feedback/feedback.py
@@ -1,147 +1,69 @@
-import discord 
-from redbot.core import commands
-from redbot.core import Config, config, checks 
-import random
-import time
-from typing import Literal
-import asyncio
-from redbot.core.utils.chat_formatting import (bold, box, humanize_list, humanize_number, pagify)
-from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
-from redbot.core.utils.predicates import MessagePredicate
+import discord
+import json
+from discord.ext import commands
+intents = discord.Intents.all()
+intents.message_content = True
+intents.messages = True
+intents = discord.Intents.all()
 
-class Feedback(commands.Cog):
-    """Track Feedback Management Cog. Recognize track feedback requests, and reward members for providing meaningful feedback."""
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-    def __init__(self, bot):
-        """Function that initializes the cog.
-        """
-        self.bot = bot
-        self.config = Config.get_conf(self, 234256122784481002, force_registration=True)
-        
+required_words = ['drums', 'snare', 'low', 'high', 'mix', 'compression', 'womp', 'burrrrrrrrr', 'transient', 'reese']
+min_characters = 280
 
-        # Registering the default values for the server bot
-        default_guild = {
-                    "feedback_min_length": 148, #minimum length to qualify as feedback
-                    "react_to_award": True, #admin reacts to award
-                    "word_to_award": True, #admin writes to award
-                    "channels": [], #channels where feedback will be listening
-                    "feedback_reward": True #Turn feedback rewards on or off
-                    } 
+#Functions to load and save karma data to json
+def load_karma_data():
+    with open('karma.json', 'r') as f:
+        return json.load(f)
+    
+def save_karma_data(data):
+    with open('karma.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
-        default_global = {
-                    "feedback_enabled": True #enable feedback by default
-                    }
-# Registering the default values for the config.
-        default_user = {"tokens": 0}
-        self.config.register_user(**default_user)
-        self.config.register_guild(**default_guild)
-        self.config.register_global(**default_global)
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is now enforcing feedback!')
 
-    @commands.guild_only()
-    @commands.group()
-    async def feedback(self, ctx):
-        """Admin functions for Feedback."""
-        # Reward tokens or adjust module settings
-        if ctx.invoked_subcommand is None:
-            guild_data = await self.config.guild(ctx.guild).all()
-        if not guild_data["channels"]:
-            channel_names = ["No channels set."]
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.channel.name == 'bot-dev-forum':
+        if message.reference:
+            initial_message = await message.channel.fetch_message(message.reference.message_id)
+            if initial_message.author != bot.user and message.author != initial_message.author:  # Add the check here
+                data = load_karma_data()
+                thread_id = str(initial_message.id)
+                user_id = str(message.author.id)
+
+                if thread_id in data['threads']:
+                    if user_id not in data['threads'][thread_id]:
+                        data['threads'][thread_id].append(user_id)
+
+                        if user_id in data['users']:
+                            data['users'][user_id] += 1
+                        else:
+                            data['users'][user_id] = 1
+
+                        save_karma_data(data)
+                        await message.channel.send(f"{message.author.mention} has earned 1 feedback karma point!")
         else:
-            channel_names = []
-            for channel_id in guild_data["channels"]:
-                channel_obj = self.bot.get_channel(channel_id)
-                if channel_obj:
-                    channel_names.append(channel_obj.name)
+            if len(message.content) < min_characters:
+                await message.delete()
+                await message.channel.send(f"{message.author.mention}, your feedback request must be at least {min_characters} characters long.")
 
-        feedback_mode_react = "React" if guild_data["react_to_award"] else "Off"
-        feedback_mode_words = "Words" if guild_data["word_to_award"] else "Off"
-        feedback_reward = "On" if guild_data["feedback_reward"] else "Off"
-        feedback_min_length = guild_data["feedback_min_length"]
-
-        msg = f"[Feedback Active in]:                 {humanize_list(channel_names)}\n"
-        msg += f"[Feedback Rewards]:               {guild_data['feedback_reward']}\n"
-        msg += f"[Feedback Mode React]:      {guild_data['react_to_award']}\n"
-        msg += f"[Feedback Mode Words]:      {guild_data['word_to_award']}\n"
-
-        for page in pagify(msg, delims=["\n"]):
-            await ctx.send(box(page, lang="ini"))
-
-
-    @feedback.command()
-    async def get_tokens(self, ctx: commands.Context, member: discord.Member = None):
-        """Get Balance of Feedback Tokens for a Member."""
-        if member:
-            if member.id == self.bot.user.id:
-                member = ctx.message.author
-                bot_msg = [
-                    (
-                        " The bot doesn't operate with Tokens."
-                    ),]
-                await ctx.send(f"{ctx.author.mention}{random.choice(bot_msg)}")
-            else: 
-                user_data = await self.config.user(member).all()
-                score = await self.config.user(member).tokens()
-                await ctx.send(member.mention +f" has {score} Feedback Tokens.")
-
-    @feedback.command()
-    async def award_token(self, ctx: commands.Context, member: discord.Member = None):
-        """Award Tokens for Feedback."""
-        # Awards default token reward amount to the user config
-        # This is checking if the member is the bot. If it is, it will send a message to the user.
-        if member:
-            if member.id == self.bot.user.id:
-                member = ctx.message.author
-                bot_msg = [
-                    (
-                        " You cannot award Tokens to the bot."
-                    ),]
-
-                await ctx.send(f"{ctx.author.mention}{random.choice(bot_msg)}")
+            elif not any(word in message.content.lower() for word in required_words):
+                await message.delete()
+                await message.channel.send(f"{message.author.mention}, your feedback does not meet the municipal standard. Please provide meaningful feedback.")
+    #Requirements met, make the feedback post
             else:
-                # Adding a token to the user's token count.
-                user_data = await self.config.user(member).all()
-                user_data["tokens"] += 1
-                await self.config.user(member).set_raw(value=user_data)
-                await ctx.send(member.mention +"has been awarded a Token for giving Feedback!")
-                score = await self.config.user(member).tokens()
-                await ctx.send(member.mention +f"now has {score} Tokens.")
-                
-        else:
-            await ctx.send(f'Tokens must be awarded to a user.')
-    @checks.mod_or_permissions(manage_guild=True)
-    @feedback.command()
-    async def start(self, ctx, channel: discord.TextChannel = None):
-        """Start AutoFX Feedback"""
-        if not channel:
-            channel = ctx.channel
+                print("Message meets requirements")  # Debug print
+                data = load_karma_data()
+                thread_id = str(message.id)
+                data['threads'][thread_id] = []
+                save_karma_data(data)
 
-        if not channel.permissions_for(ctx.guild.me).send_messages:
-            return await ctx.send(bold("I can't send messages in that channel!"))
+    await bot.process_commands(message)
 
-        channel_list = await self.config.guild(ctx.guild).channels()
-        if channel.id in channel_list:
-            message = f"AutoFX Feedback already started in {channel.mention}!"
-        else:
-            channel_list.append(channel.id)
-            message = f"AutoFX Feedback started in {channel.mention}. Post your tracks to get feedback!"
-            await self.config.guild(ctx.guild).channels.set(channel_list)
-
-        await ctx.send(bold(message))
-
-    @checks.mod_or_permissions(manage_guild=True)
-    @feedback.command()
-    async def stop(self, ctx, channel: discord.TextChannel = None):
-        """Stop AutoFX Feedback."""
-        if not channel:
-            channel = ctx.channel
-        channel_list = await self.config.guild(ctx.guild).channels()
-
-        if channel.id not in channel_list:
-            message = f"AutoFX Feedback Disabled in {channel.mention}!"
-        else:
-            channel_list.remove(channel.id)
-            message = f"AutoFX Feedback has been stopped in {channel.mention}."
-            await self.config.guild(ctx.guild).channels.set(channel_list)
-
-        await ctx.send(bold(message))
-
+bot.run('MTA0MzU2NzM1ODExNzE2NzE4NA.G_xNgy._BIMa0RSxt88e9ibyM6GsNoeB4hrX2pjiKQO8s')
