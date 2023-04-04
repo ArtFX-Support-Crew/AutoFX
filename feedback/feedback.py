47e83121ede6dda0bb629fbb5955d41f5d9e4ee7
ropeadope62 <davechadwick@gmail.com>
2023-04-04 00:51:22 -0400

    fix

--- feedback/feedback.py
+++ feedback/feedback.py
@@ -1,80 +1,75 @@
-import discord 
-import random
-from redbot.core import commands
-from redbot.core import Config
+import discord
+import json
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+intents = discord.Intents.all()
+intents.message_content = True
+intents.messages = True
+intents = discord.Intents.all()
 
-class Feedback(commands.Cog):
-    """Track Feedback Management Cog. Recognize track feedback requests, and reward members for providing meaningful feedback."""
 
-    def __init__(self, bot):
-        self.config = Config.get_conf(self, 2938473984732, force_registration=True)
-        self.bot = bot
 
-        default_guild = {
-                    "feedback_min_length": 148, #minimum length to qualify as feedback
-                    "react_to_award": True, #admin reacts to award
-                    "word_to_award": False, #admin writes to award
-                    "channels": ['feedback'], #channels where feedback will be listening
-                    "feedback_reward": 1
-                    } #number of tokens to award 
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-        default_global = {
-                    "feedback_enabled": True #enable feedback by default
-                    }
-        default_user = {"tokens": 0, "feedbackreceived": 0, "feedbackgiven": 0}
-        self.config.register_guild(**default_guild)
-        self.config.register_user(**default_user)
-        self.config.register_guild(**default_guild)
-        self.config.register_global(**default_global)
+required_words = ['drums', 'snare', 'low', 'high', 'mix', 'compression', 'womp', 'burrrrrrrrr', 'transient', 'reese']
+min_characters = 280
 
-    @commands.command()
-    async def feedback_admin(self, ctx):
-        """Admin functions for Feedback."""
-        # Reward tokens or adjust module settings
-        await ctx.send("Feedback Admin options")
+#Functions to load and save karma data to json
+def load_karma_data():
+    with open('karma.json', 'r') as f:
+        return json.load(f)
+    
+def save_karma_data(data):
+    with open('karma.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
-    @commands.command()
-    async def award_token(self, ctx: commands.Context, user: discord.Member = None) -> None:
-        """Award Tokens for Feedback."""
-        # Awards default token reward amount to the user config
-        if user:
-            if user.id == self.bot.user.id:
-                user = ctx.message.author
-                bot_msg = [
-                    (
-                        " You cannot award tokens to the bot."
-                    )
-                ]
-                await ctx.send(f"{ctx.author.mention}{random.choice(bot_msg)}")
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is now enforcing feedback!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.channel.name == 'bot-dev-forum':
+        if message.reference:
+            initial_message = await message.channel.fetch_message(message.reference.message_id)
+            if initial_message.author != bot.user and message.author != initial_message.author:  # Add the check here
+                data = load_karma_data()
+                thread_id = str(initial_message.id)
+                user_id = str(message.author.id)
+
+                if thread_id in data['threads']:
+                    if user_id not in data['threads'][thread_id]:
+                        data['threads'][thread_id].append(user_id)
+
+                        if user_id in data['users']:
+                            data['users'][user_id] += 1
+                        else:
+                            data['users'][user_id] = 1
+
+                        save_karma_data(data)
+                        await message.channel.send(f"{message.author.mention} has earned 1 feedback karma point!")
+        else:
+            if len(message.content) < min_characters:
+                await message.delete()
+                await message.channel.send(f"{message.author.mention}, your feedback request must be at least {min_characters} characters long.")
+
+            elif not any(word in message.content.lower() for word in required_words):
+                await message.delete()
+                await message.channel.send(f"{message.author.mention}, your feedback does not meet the municipal standard. Please provide meaningful feedback.")
+    #Requirements met, make the feedback post
             else:
+                print("Message meets requirements")  # Debug print
+                data = load_karma_data()
+                thread_id = str(message.id)
+                data['threads'][thread_id] = []
+                save_karma_data(data)
 
-                user_data = await self.config.user(member).all()
-                if not member:
-                    member = ctx.author
-                user_data["tokens"] + ["feedback_reward"]
-                await ctx.send(user.mention +"has been awarded a Token for giving Feedback!")
-                score = await self.config.user(member).score()
-                await ctx.send(user.mention +f"now has {score} tokens!")
+    await bot.process_commands(message)
 
-    @commands.command()
-    async def token_balance(self, ctx: commands.Context, user: discord.Member = None) -> None:
-        """Returns the number of tokens owned by the user."""
-        # Awards default token reward amount to the user config
-        if user:
-            if user.id == self.bot.user.id:
-                user = ctx.message.author
-                bot_msg = [
-                    (
-                        " I do not give feedback, therefore I do not need tokens."
-                    ),
-                    (
-                        " The token_balance must belong to a user, not a bot."
-                    )
-                ]
-                await ctx.send(f"{ctx.author.mention}{random.choice(bot_msg)}")
-            else:
-                user_data = await self.config.user(member).all()
-                if not member:
-                    member = ctx.author
-                tokens = user_data["tokens"]
-                await ctx.send(user.mention(f"has {tokens} feedback tokens remaining."))
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot.run(BOT_TOKEN)
