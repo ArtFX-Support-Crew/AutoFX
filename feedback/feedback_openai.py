69fe30123dd05fb559168bd10a1048a1e82160e7
ropeadope62 <davechadwick@gmail.com>
2023-05-15 03:41:08 -0400

    Update feedback_openai.py

--- feedback/feedback_openai.py
+++ feedback/feedback_openai.py
@@ -4,7 +4,7 @@ import time
 class openAI: 
     def feedback_ai(self):
 
-        openai.api_key = "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"
+        openai.api_key = ""
         try:
             # Check if the message qualifies as meaningful feedback
             response = openai.Completion.create(
